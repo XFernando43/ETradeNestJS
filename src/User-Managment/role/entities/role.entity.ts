7b0e338ac17ff44870dc76048f0deac44fb62abc
unknown <U20171E997@upc.edu.pe>
2024-03-20 16:41:04 +0100

    add: role entity

--- src/User-Managment/role/entities/role.entity.ts
+++ src/User-Managment/role/entities/role.entity.ts
@@ -1,1 +1,9 @@
-export class Role {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Roles")
+export class Role {
+    @PrimaryGeneratedColumn()
+    roleId:number;
+    @Column()
+    roleName:string;
+}
