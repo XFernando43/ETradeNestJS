b35876c5f6d44e26ba83a7553b860decb7b74e12
unknown <U20171E997@upc.edu.pe>
2024-03-20 16:38:37 +0100

    add: users entity

--- src/User-Managment/users/entities/user.entity.ts
+++ src/User-Managment/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Users")
+export class User {
+    @PrimaryGeneratedColumn()
+    userId:number;
+    @Column()
+    username:string;
+    @Column()
+    roleId:number;
+}
