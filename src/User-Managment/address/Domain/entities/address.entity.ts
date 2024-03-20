bddb95ef124278dc2a414557e64c0413070feffa
unknown <U20171E997@upc.edu.pe>
2024-03-20 16:37:08 +0100

    add: users address entity

---
+++ src/User-Managment/address/Domain/entities/address.entity.ts
@@ -0,0 +1,16 @@
+import { User } from "src/User-Managment/users/entities/user.entity";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Address")
+export class Address {
+
+    @PrimaryGeneratedColumn()
+    addressId:number;
+    @Column()
+    addressName:string;
+    @Column()
+    addressIP: string;
+
+    User:User
+
+}
