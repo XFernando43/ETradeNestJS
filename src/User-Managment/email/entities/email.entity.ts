549b2988c79f3d22fd15db631fbfb0f1e722b9b2
unknown <U20171E997@upc.edu.pe>
2024-03-21 03:15:42 -0800

    add: add email entity

--- src/User-Managment/email/entities/email.entity.ts
+++ src/User-Managment/email/entities/email.entity.ts
@@ -1,1 +1,16 @@
-export class Email {}
+import { User } from "src/User-Managment/users/entities/user.entity";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Emails")
+export class Email {
+    @PrimaryGeneratedColumn()
+    emailId: number;
+    @Column()
+    emailAddres: string;
+    @Column()
+    password: string;
+
+    @OneToOne(()=> User)
+    @JoinColumn({name:'UserId'})
+    user:User;
+}
