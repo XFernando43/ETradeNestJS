45123b6dc350e0d954872865d7e66596f0e770fa
XFernando43 <U20171E997@upc.edu.pe>
2024-03-24 20:44:45 -0500

    refactor: all user-managment

--- src/User-Managment/Application/Service/users.service.ts
+++ src/User-Managment/Application/Service/users.service.ts
@@ -14,108 +14,154 @@ import { CreateEmailDto } from 'src/User-Managment/Domain/dto/email/create-email
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private roleRepository: RoleService,
+    private emailRepository: EmailService,
+    private CartRepository: CartService,
+  ) {}
 
-  constructor(@InjectRepository(User) private userRepository:Repository<User>,
-              private roleRepository:RoleService,
-              private emailRepository: EmailService,
-              private CartRepository:CartService,
-              ){}
-
-  async create(createUserDto: CreateUserDto, roleId:number) {
-    try{
-
+  async create(createUserDto: CreateUserDto, roleId: number) {
+    try {
       let roleFinded = await this.roleRepository.findOne(roleId);
-      const newUser = new User(createUserDto.name,createUserDto.username,createUserDto.lastName,createUserDto.bornDate,roleFinded);
+      const newUser = new User(
+        createUserDto.name,
+        createUserDto.username,
+        createUserDto.lastName,
+        createUserDto.bornDate,
+        roleFinded,
+      );
       const userDb = await this.userRepository.create(newUser);
-      await this.userRepository.save(userDb);      
-      const newEmail = new CreateEmailDto(createUserDto.email,createUserDto.password,newUser.userId);      
+      await this.userRepository.save(userDb);
+      const newEmail = new CreateEmailDto(
+        createUserDto.email,
+        createUserDto.password,
+        newUser.userId,
+      );
       await this.emailRepository.create(newEmail);
       const newCart = new Cart(userDb);
       const cartDb = await this.CartRepository.create(newCart);
       return {
         status: HttpStatus.CREATED,
         user: newUser,
-        email:newEmail,
-        cart:cartDb
-      }
-    }catch(error){
+        email: newEmail,
+        cart: cartDb,
+      };
+    } catch (error) {
       console.error('Error creating user:', error);
-      throw new HttpException('Error creating user', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        'Error creating user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async findAll() {
-    try{
+    try {
       const users = await this.userRepository.find({ relations: ['role'] });
       return {
-        status:201,
-        users: users
-      }
-    }catch(error){
+        status: 201,
+        users: users,
+      };
+    } catch (error) {
       console.log(error);
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-  
+
   async findOne(id: number) {
-    try{
-      const user = await this.userRepository.findOne({where:{userId:id}, relations:["role"]});
+    try {
+      const user = await this.userRepository.findOne({
+        where: { userId: id },
+        relations: ['role'],
+      });
       if (!user) {
-        throw new HttpException(`User with ID ${id} not found`, HttpStatus.NOT_FOUND);
+        throw new HttpException(
+          `User with ID ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
       }
       return {
-        status:HttpStatus.OK,
-        user: user
-      }
-    }catch(error){
+        status: HttpStatus.OK,
+        user: user,
+      };
+    } catch (error) {
       console.error('Error finding all users:', error);
-      throw new HttpException('Error finding all users', HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        'Error finding all users',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    try{
+    try {
       const userFinded = await this.userRepository.findOne({
-        where:{
-          userId:id
-        }
-      })
-      if(!userFinded){
-        throw new HttpException(`User with ID ${id} not found`, HttpStatus.NOT_FOUND);
-      }else{
+        where: {
+          userId: id,
+        },
+      });
+      if (!userFinded) {
+        throw new HttpException(
+          `User with ID ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
+      } else {
         let userUpdated;
-        if(updateUserDto.roleId){ 
-          const roleFinded = await this.roleRepository.findOne(updateUserDto.roleId);
-          userUpdated = new UpdateUserDto(updateUserDto.name,updateUserDto.username,updateUserDto.lastName,roleFinded);
+        if (updateUserDto.roleId) {
+          const roleFinded = await this.roleRepository.findOne(
+            updateUserDto.roleId,
+          );
+          userUpdated = new UpdateUserDto(
+            updateUserDto.name,
+            updateUserDto.username,
+            updateUserDto.lastName,
+            roleFinded,
+          );
         }
-        userUpdated = new UpdateUserDto(updateUserDto.name,updateUserDto.username,updateUserDto.lastName);
-        await this.userRepository.update(id,userUpdated);
+        userUpdated = new UpdateUserDto(
+          updateUserDto.name,
+          updateUserDto.username,
+          updateUserDto.lastName,
+        );
+        await this.userRepository.update(id, userUpdated);
         return {
           status: HttpStatus.OK,
-          user: updateUserDto
-        }  
+          user: updateUserDto,
+        };
       }
-    }catch(error){
+    } catch (error) {
       console.error(`Error updating user with ID ${id}:`, error);
-      throw new HttpException(`Error updating user with ID ${id}`, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        `Error updating user with ID ${id}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async remove(id: number) {
-    try{
-      const userFinded = await this.userRepository.findOne({where:{userId:id}});
-      if(userFinded){
+    try {
+      const userFinded = await this.userRepository.findOne({
+        where: { userId: id },
+      });
+      if (userFinded) {
         await this.userRepository.delete(id);
         return {
           status: HttpStatus.OK,
           message: `User with ID ${id} deleted successfully`,
         };
-      }else{
-        throw new HttpException(`User with ID ${id} not found`, HttpStatus.NOT_FOUND);
+      } else {
+        throw new HttpException(
+          `User with ID ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
       }
-    }catch(error){
+    } catch (error) {
       console.error(`Error removing user with ID ${id}:`, error);
-      throw new HttpException(`Error removing user with ID ${id}`, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException(
+        `Error removing user with ID ${id}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
